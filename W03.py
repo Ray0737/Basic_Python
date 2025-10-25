3e903b0026b778205223eb029eea3d17d4e2c121
c0der <raphee.rattanamanoonporn@gmail.com>
2025-10-25 19:41:03 +0700

    Update W03.py

--- W03.py
+++ W03.py
@@ -1,4 +1,8 @@
-#Exercise 1-5 Lv: Moderate
+'''
+Week 03 | Data types & Data structures
+'''
+
+#Exercise 1-5 
 student_num = [1,2,3,4,5,6,7,8,9,10]
 student_name = ["Zihan","Pair","Namjai","Peter","Aomsin","Im","Tigger","Jay","Tangman","Winston"]
 print("Number","Name",sep = '\t')
@@ -18,20 +22,20 @@ print(student_name[-1])
 print(student_name[4])
 print(student_name[3])
 
-#Exercise 6 Lv: Moderate
+#Exercise 6 
 list1 = [0,1,2,'a','b','c']
 print(list1[0])
 
-#Exercise 7 Lv: Moderate
+#Exercise 7 
 list2 = ['ant','bird','cat','dog','eagle']
 list2[3] = "duck"
 print(list2)
 
-#Exercise 8 Lv: Moderate
+#Exercise 8 
 list5 = [1,2,3,4,5,'a','f']
 print(len(list5))
 
-#Exercise 9 Lv: Moderate
+#Exercise 9 
 listy = []
 text1 = input("input any string: ")
 text2 = input("input any string: ")
@@ -41,15 +45,16 @@ listy.append(text2)
 listy.append(text3)
 print(listy)
 
-#Exercise 10 Lv: Moderate
+#Exercise 10 
 list4 = [0,4,2,3,1]
 list4.sort()
 print(list4)
 
-#Exercise 11 Lv: Moderate
+#Exercise 11 
 listx = ["VISIONAI","CHATAI","GAMEAI"]
 model4 = input("Enter the name of the 4th model: ")
 model5 = input("Enter the name of the 5th model: ")
 listx.append(model4)
 listx.append(model5)
+
 print(listx)
