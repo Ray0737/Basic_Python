64958225226a18cc8dc00cc23b88c2930fc3884c
c0der <raphee.rattanamanoonporn@gmail.com>
2025-10-25 19:36:14 +0700

    Update W01.py

--- W01.py
+++ W01.py
@@ -1,33 +1,38 @@
-#Q1 Lv: Moderate
+'''
+Week 01 | Introduction - String, Variable, Print command
+'''
+
+#Q1 
 a=2
 b=3
 print("%d + %d = %d + %d = %d" %(a,b,b,a,a+b))
 
-#Q2 Lv: Moderate
+#Q2 
 a=2
 b=3
 c=5
 print("%d*(%d+%d) = %d*%d+%d*%d" %(a,b,c,a,b,a,c))
 
-#Q3 Lv: Moderate
+#Q3 
 a=2.4
 b=2.5
 print("%.2f - %.2f = %.2f" %(b,a,b-a))
 
-#Q4 Lv: Moderate
+#Q4 
 birthday = 25
 print("ฉันเกิดวันที่ %d ธันวาคม" %(birthday))
 
-#Q5 Lv: Moderate
+#Q5 
 a=5
 b = "%"
 print("ฉันได้กำไร %d%s"%(a,b))
 
-#Q6 Lv: Moderate
+#Q6 
 a=2
 b=3.5
 x= '%'
 print("เมื่อวานฉันขาดทุน %d%s วันนี้ฉันได้กำไร %.2f%s"%(a,x,b,x))
+
 
 
 
