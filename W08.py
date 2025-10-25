29f5dc439638d56181ab95fc3151fa1719665f8a
c0der <raphee.rattanamanoonporn@gmail.com>
2025-10-25 19:47:31 +0700

    Refactor and uncomment Employee and Businessman classes

--- W08.py
+++ W08.py
@@ -1,39 +1,43 @@
-# class Employee:
-#     def __init__(self) -> None:
-#         print("Default Constructor")
-#     def __init__(self, name, salary):
-#         self.name = name
-#         self.salary = salary
-#     def edit_salary(self, name, salary):
-#         self.name = name
-#         self.salary = salary
-#     def showdata(self):
-#         print(f"Name: {self.name}")
-#         print(f"Salary: {self.salary}")
-# emp1 = Employee("John Doe", 50000)
-# emp1.showdata()
-# emp1.edit_salary("Jane Doe", 60000)
-# emp1.showdata()
+'''
+Week 08 | Object-Oriented Programming
+'''
 
-# class animal: 
-#     def showdata(self):
-#         print("Aomsin")
+class Employee:
+    def __init__(self) -> None:
+        print("Default Constructor")
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+    def edit_salary(self, name, salary):
+        self.name = name
+        self.salary = salary
+    def showdata(self):
+        print(f"Name: {self.name}")
+        print(f"Salary: {self.salary}")
+emp1 = Employee("John Doe", 50000)
+emp1.showdata()
+emp1.edit_salary("Jane Doe", 60000)
+emp1.showdata()
 
-# class fighterjet:
-#     def detail(self,x):
-#         self.name = "F22" and "Raptor"
-#         self.missile = "AIM-120" and "AIM-9"
-#         self.cannon = "M61A2"
-#         self.radarcrosssection = 0.001
-#         self.killcount= x
-#         print(f"Aircraft:{self.name}")
-#         print(f"Missile: {self.missile} ")
-#         print(f"Aircraft:{self.cannon}")
-#         print(f"Aircraft:{self.radarcrosssection}")
-#         print(f"Aircraft:{self.killcount}")
+class animal: 
+    def showdata(self):
+        print("Aomsin")
 
-# Raptor = fighterjet()
-# Raptor.detail(1)
+class fighterjet:
+    def detail(self,x):
+        self.name = "F22" and "Raptor"
+        self.missile = "AIM-120" and "AIM-9"
+        self.cannon = "M61A2"
+        self.radarcrosssection = 0.001
+        self.killcount= x
+        print(f"Aircraft:{self.name}")
+        print(f"Missile: {self.missile} ")
+        print(f"Aircraft:{self.cannon}")
+        print(f"Aircraft:{self.radarcrosssection}")
+        print(f"Aircraft:{self.killcount}")
+
+Raptor = fighterjet()
+Raptor.detail(1)
 
 
 class Businessman:
@@ -62,4 +66,5 @@ man1.showdata()
 man1.sleep()
 man1.showdata()
 man1.play()
+
 man1.showdata()
