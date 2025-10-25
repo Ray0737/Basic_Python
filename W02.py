665ecac8f629342815829fe3acaf7f508baa858d
c0der <raphee.rattanamanoonporn@gmail.com>
2025-10-25 19:38:31 +0700

    Update W02.py

--- W02.py
+++ W02.py
@@ -1,14 +1,18 @@
-# Exercise 1-2 Lv: Moderate
+'''
+Week 02 | Input command
+'''
+
+# Exercise 1-2 
 a = input("Enter your sth")
 x = type(a)
 print(f"{a} {x}")
 
-# Exercise 3-4 Lv: Moderate
+# Exercise 3-4 
 name = input("Enter your name")
 x = type(name)
 print(f"your name is {name} \nYour have enter the value in {x}")
 
-# Exercise 5 Lv: Moderate
+# Exercise 5
 num1 = float(input("enter a number"))
 num2 = float(input("enter a number"))
 addition = num1 + num2
@@ -24,7 +28,7 @@ print(f"{num1} - {num2} = {subtraction} \nYour ans has the value in {y}")
 print(f"{num1} x {num2} = {multiplication} \nYour ans has the value in {z}")
 print(f"{num1} ÷ {num2} = {division} \nYour ans has the value in {a}")
 
-# Exercise 6 Lv: Moderate
+# Exercise 6 
 a = float(input("Enter a real number"))
 b = int(input("enter a integer"))
 addition = a+b
@@ -34,7 +38,7 @@ y = type(subtraction)
 print(f"{a} + {b} = {addition} \nYour ans has the value in {x}")
 print(f"{a} - {b} = {subtraction} \nYour ans has the value in {y}")
 
-# Exercise 7 Lv: Moderate
+# Exercise 7 
 force = float(input("whats the force (N)"))
 distance = float(input("Whats the distance (M)"))
 time = float(input("What's the time of the action"))
@@ -42,24 +46,25 @@ work = force*distance
 power = work/time
 print(f"your work is {work} Jules \nand the total power is {power} Watt")
 
-# Exercise 8-9 Lv: Moderate
+# Exercise 8-9 
 firstname = input("enter your firstname")
 surname = input("Enter your surname")
 age = int(input("Enter your age ininteger"))
 age_sister = int(input("Enter your sister's age ininteger"))
 print(f"your name is {firstname} {surname}, and you're {age} years old, and you have a sister withe the age of {age_sister} years old, and the age gap is {age - age_sister}")
 
-#Exercise 10-12 Lv: Moderate
+#Exercise 10-12 
 x = float(input("enter a number"))
 y = float(input("enter a number"))
 result_1 = x>y
 print(bool(result_1))
 print(type(result_1))
 
-# Exercise 13 Lv: Moderate
+# Exercise 13 
 import math
 radius = float(input("Enter the radius of the base of the cone (Cm)"))
 height = float(input("Enter the height of your cone (Cm)"))
 volume = (1/3)*math.pi*(radius**2)*height
 print(f"The volume of your cone is {volume} Cubic Centimeter")
+
 
